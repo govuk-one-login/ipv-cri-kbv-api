33e74d83bf908f73e0ea57e04fb9a8e68d3f3706
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-01-19 16:53:05 +0000

    wip on handling the call to get questions or submit answers

--- src/main/java/uk/gov/di/cri/experian/kbv/api/resource/KBVResource.java
+++ src/main/java/uk/gov/di/cri/experian/kbv/api/resource/KBVResource.java
@@ -6,18 +6,22 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 import uk.gov.di.cri.experian.kbv.api.domain.PersonIdentity;
+import uk.gov.di.cri.experian.kbv.api.domain.QuestionAnswer;
+import uk.gov.di.cri.experian.kbv.api.domain.QuestionAnswerRequest;
 import uk.gov.di.cri.experian.kbv.api.domain.QuestionState;
 import uk.gov.di.cri.experian.kbv.api.domain.QuestionsResponse;
 import uk.gov.di.cri.experian.kbv.api.service.StorageService;
 
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class KBVResource {
 
@@ -49,39 +53,112 @@ public class KBVResource {
 
                 String json = objectMapper.writeValueAsString(personIdentity);
 
-                System.out.println(json);
-
-                URI wrapperResourceURI = getKBVWrapperURI();
-                System.out.println("👍 wrapper uri:" + wrapperResourceURI);
-                HttpRequest httpReq =
-                        HttpRequest.newBuilder()
-                                .uri(wrapperResourceURI)
-                                .setHeader("Accept", "application/json")
-                                .setHeader("Content-Type", "application/json")
-                                .POST(HttpRequest.BodyPublishers.ofString(json))
-                                .build();
-
-                HttpClient httpClient = HttpClient.newHttpClient();
-                HttpResponse<String> res =
-                        httpClient.send(
-                                httpReq, java.net.http.HttpResponse.BodyHandlers.ofString());
-
-                // we get x questions back
-                // deser
-
-                QuestionsResponse questionsResponse =
-                        objectMapper.readValue(res.body(), QuestionsResponse.class);
-                questionState.setQuestionsResponse(questionsResponse);
-                storageService.save(sessionId, questionState);
                 Question question = questionState.getNextQuestion();
+                if (question == null) {
+                    // fetch a batch of questions
+                    HttpResponse<String> res = getMoreQuestions(json);
+                    QuestionsResponse questionsResponse =
+                            objectMapper.readValue(res.body(), QuestionsResponse.class);
+                    questionState.setQuestionsResponse(questionsResponse);
+                    storageService.save(sessionId, questionState);
+                    question = questionState.getNextQuestion();
+
+                }
                 response.status(HttpServletResponse.SC_OK);
                 response.type("application/json;charset=UTF-8");
                 return objectMapper.writeValueAsString(question);
+
             };
 
-    private URI getKBVWrapperURI() {
+
+    public final Route answer =
+            (Request request, Response response) -> {
+
+                String sessionId = request.headers("session-id");
+                QuestionState questionState = storageService.get(sessionId);
+
+
+                QuestionAnswer answer = objectMapper.readValue(request.body(), QuestionAnswer.class);
+
+                questionState.setAnswer(answer);
+
+                if (questionState.submitAnswers()) {
+
+                    QuestionAnswerRequest questionAnswerRequest = new QuestionAnswerRequest();
+                    List<QuestionState.QuestionAnswerPair> pairs = questionState.getQas();
+
+                    List<QuestionAnswer> collect = pairs.stream().map(pair ->
+                            {
+                                QuestionAnswer questionAnswer = new QuestionAnswer();
+                                questionAnswer.setAnswer(pair.getAnswer());
+                                questionAnswer.setQuestionId(pair.getQuestion().getQuestionID());
+                                return questionAnswer;
+                            }
+                    ).collect(Collectors.toList());
+
+
+                    questionAnswerRequest.setQuestionAnswers(collect);
+                    questionAnswerRequest.setAuthRefNo(questionState.getControl().getAuthRefNo());
+                    questionAnswerRequest.setUrn(questionState.getControl().getURN());
+                    String json = objectMapper.writeValueAsString(questionAnswerRequest);
+
+                    URI wrapperResourceURI = get_KBV_RTQ_URI();
+                    System.out.println("👍 wrapper uri:" + wrapperResourceURI);
+                    HttpRequest httpReq =
+                            HttpRequest.newBuilder()
+                                    .uri(wrapperResourceURI)
+                                    .setHeader("Accept", "application/json")
+                                    .setHeader("Content-Type", "application/json")
+                                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                                    .build();
+
+                    HttpClient httpClient = HttpClient.newHttpClient();
+                    HttpResponse<String> res =
+                            httpClient.send(
+                                    httpReq, HttpResponse.BodyHandlers.ofString());
+
+                    QuestionsResponse questionsResponse =
+                            objectMapper.readValue(res.body(), QuestionsResponse.class);
+
+                    questionState.setQuestionsResponse(questionsResponse);
+
+                }
+
+
+                response.status(HttpServletResponse.SC_NO_CONTENT);
+                response.type("application/json;charset=UTF-8");
+                return null;
+
+            };
+
+
+    private HttpResponse<String> getMoreQuestions(String json) throws IOException, InterruptedException {
+        URI wrapperResourceURI = get_KBV_SAA_URI();
+        System.out.println("👍 wrapper uri:" + wrapperResourceURI);
+        HttpRequest httpReq =
+                HttpRequest.newBuilder()
+                        .uri(wrapperResourceURI)
+                        .setHeader("Accept", "application/json")
+                        .setHeader("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(json))
+                        .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> res =
+                httpClient.send(
+                        httpReq, HttpResponse.BodyHandlers.ofString());
+        return res;
+    }
+
+    private URI get_KBV_SAA_URI() {
         String baseURL = System.getenv("EXPERIAN_API_WRAPPER_URL");
         String resource = System.getenv("EXPERIAN_API_WRAPPER_SAA_RESOURCE");
+        return URI.create(baseURL + resource);
+    }
+
+    private URI get_KBV_RTQ_URI() {
+        String baseURL = System.getenv("EXPERIAN_API_WRAPPER_URL");
+        String resource = System.getenv("EXPERIAN_API_WRAPPER_RTQ_RESOURCE");
         return URI.create(baseURL + resource);
     }
 }
