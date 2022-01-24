71c37fb780fec744c8e268d60600e8322d3d57e5
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2022-01-24 13:55:49 +0000

    KBV-90: Removed SOAP webservice classes from the KBV Experian API
    Co-authored-by: Aman Mohammed <aman.mohammed@digital-cabinet.office.gov.uk>

--- src/main/java/uk/gov/di/cri/experian/kbv/api/App.java
+++
@@ -1,9 +0,0 @@
-// package uk.gov.di.cri.experian.kbv.api;
-//
-// import java.io.IOException;
-//
-// public class App {
-//    public static void main(String[] args) throws IOException {
-//        new KBVApi();
-//    }
-// }
--- src/main/java/uk/gov/di/cri/experian/kbv/api/domain/Question.java
+++ src/main/java/uk/gov/di/cri/experian/kbv/api/domain/Question.java
@@ -1,3 +1,9 @@
 package uk.gov.di.cri.experian.kbv.api.domain;
 
-public class Question {}
+public class Question {
+    private String questionID;
+
+    public String getQuestionID() {
+        return questionID;
+    }
+}
