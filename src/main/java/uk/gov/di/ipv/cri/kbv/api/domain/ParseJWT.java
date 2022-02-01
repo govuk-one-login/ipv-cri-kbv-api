354b4c24cbf302ac9aca6e9dfcf924a42f3de807
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2022-02-01 11:43:37 +0000

    KBV-90: Implement JWT sessionhander/ParseJWT
    KBV-90: Implemented unit tests for JWT in SessionHandler
    KBV-90: Added JUnits for ParseJWTTest.java
    
    Co-authored-by: Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/ipv/cri/kbv/api/domain/ParseJWT.java
+++ src/main/java/uk/gov/di/ipv/cri/kbv/api/domain/ParseJWT.java
@@ -1,9 +1,36 @@
 package uk.gov.di.ipv.cri.kbv.api.domain;
 
-import java.util.Map;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.nimbusds.jwt.SignedJWT;
+
+import java.text.ParseException;
+import java.util.Optional;
 
 public class ParseJWT {
-    public short parseHeaderJWT(Map<String, String> jwtHeader) {
-        return 0;
+
+    private final ObjectMapper objectMapper;
+
+    public ParseJWT(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public Optional<PersonIdentity> getPersonIdentity(APIGatewayProxyRequestEvent input)
+            throws ParseException, JsonProcessingException {
+        try {
+            String jwt = input.getQueryStringParameters().get("request");
+            SignedJWT jwtString = SignedJWT.parse(jwt);
+            String payload = jwtString.getJWTClaimsSet().getClaim("claim").toString();
+            JsonNode node = objectMapper.readTree(payload).get("vc_http_api");
+            ObjectNode objectNode = (ObjectNode) node;
+            objectNode.remove("ipv_session_id");
+            return Optional.ofNullable(
+                    objectMapper.readValue(node.toString(), PersonIdentity.class));
+        } catch (ParseException | JsonProcessingException e) {
+            throw e;
+        }
     }
 }
