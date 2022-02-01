21faabb174e92d8f8c3a15d82c07e921075a3425
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2022-02-01 11:18:12 +0000

    KBV-90: Implement code to persist to dynamodb

--- src/main/java/uk/gov/di/ipv/cri/kbv/api/service/StorageService.java
+++ src/main/java/uk/gov/di/ipv/cri/kbv/api/service/StorageService.java
@@ -1,19 +1,23 @@
 package uk.gov.di.ipv.cri.kbv.api.service;
 
-import uk.gov.di.ipv.cri.kbv.api.domain.QuestionState;
-
-import java.util.HashMap;
-import java.util.Map;
+import uk.gov.di.ipv.cri.kbv.api.persistence.DataStore;
+import uk.gov.di.ipv.cri.kbv.api.persistence.item.KBVSessionItem;
 
 public class StorageService {
+    private final DataStore<KBVSessionItem> dataStore;
 
-    private Map<String, QuestionState> map = new HashMap<>();
-
-    public void save(String key, QuestionState state) {
-        map.put(key, state);
+    public StorageService(DataStore<KBVSessionItem> datastore) {
+        this.dataStore = datastore;
     }
 
-    public QuestionState get(String sessionId) {
-        return map.get(sessionId);
+    public KBVSessionItem getSessionId(String sessionId) {
+        return dataStore.getItem(sessionId);
+    }
+
+    public void save(String sessionId, String state) {
+        KBVSessionItem kbvSessionItem = new KBVSessionItem();
+        kbvSessionItem.setSessionId(sessionId);
+        kbvSessionItem.setQuestionState(state);
+        dataStore.create(kbvSessionItem);
     }
 }
