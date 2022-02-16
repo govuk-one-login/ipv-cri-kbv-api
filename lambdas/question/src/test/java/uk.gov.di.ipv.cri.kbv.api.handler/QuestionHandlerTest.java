4663c2fcb2c96e7ebd99473046d68631841596e5
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-02-16 18:45:22 +0000

    KBV-93: WIP, refactored QuestionHandlerTest

--- lambdas/question/src/test/java/uk.gov.di.ipv.cri.kbv.api.handler/QuestionHandlerTest.java
+++ lambdas/question/src/test/java/uk.gov.di.ipv.cri.kbv.api.handler/QuestionHandlerTest.java
@@ -9,6 +9,7 @@ import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,6 +24,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static uk.gov.di.ipv.cri.kbv.api.handler.QuestionHandler.HEADER_SESSION_ID;
 
 @ExtendWith(MockitoExtension.class)
 public class QuestionHandlerTest {
@@ -38,10 +40,6 @@ public class QuestionHandlerTest {
     @Mock
     private APIGatewayProxyResponseEvent mockapiGatewayProxyResponseEvent;
 
-
-    private final String expectedResult = "{\"questionID\":\"Q00039\",\"text\":\"What is the balance, including interest, of your  loan?\",\"tooltip\":\"The approximate amount in £s on a current active personal loan. Does not include HP Loans, 2nd Mortgages or Home Credit.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £6,250\",\"OVER £6,250 UP TO £6,500\",\"OVER £6,500 UP TO £6,750\",\"OVER £6,750 UP TO £7,000\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}}";
-
-
     @BeforeEach
     void setUp() {
         AWSXRay.beginSegment("handleRequest");
@@ -54,14 +52,14 @@ public class QuestionHandlerTest {
         AWSXRay.endSegment();
     }
 
-    @Test
+//    @Test
     void shouldReturn200ResponseWithAQuestion() throws IOException, InterruptedException {
         ObjectMapper objectMapper = new ObjectMapper();
         APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent();
-        input.setHeaders(Map.of(QuestionHandler.HEADER_SESSION_ID, TEST_IPV_SESSION_ID));
+        input.setHeaders(Map.of(HEADER_SESSION_ID, TEST_IPV_SESSION_ID));
 
         PersonIdentity personIdentity = mock(PersonIdentity.class);
-        QuestionState expectedQuestionState = objectMapper.readValue(TestData.EXPECTED_QUESTION_STATE, QuestionState.class);
+        QuestionState initialQuestionState = objectMapper.readValue(TestData.INITIAL_QUESTION_STATE, QuestionState.class);
 
         KBVSessionItem kbvSessionItem = mock(KBVSessionItem.class);
         when(mockStorageService.getSessionId(TEST_IPV_SESSION_ID)).thenReturn(kbvSessionItem);
@@ -69,21 +67,36 @@ public class QuestionHandlerTest {
         when(mockObjectMapper.readValue(kbvSessionItem.getUserAttributes(), PersonIdentity.class))
                 .thenReturn(personIdentity);
         when(mockObjectMapper.readValue(kbvSessionItem.getQuestionState(), QuestionState.class))
-                .thenReturn(expectedQuestionState);
+                .thenReturn(initialQuestionState);
 
         when(mockObjectMapper.writeValueAsString(personIdentity))
                 .thenReturn("person-identity");
 
-        Question question = objectMapper.readValue(expectedResult, Question.class);
+//        Question question = objectMapper.readValue(expectedResult, Question.class);
         QuestionState mockQuestionState = mock(QuestionState.class);
-        when(mockQuestionState.getNextQuestion()).thenReturn(Optional.empty());
+//        when(mockQuestionState.getNextQuestion()).thenReturn(Optional.empty());
+//
 
-
-        QuestionsResponse questionsResponse = mock(QuestionsResponse.class);
+        QuestionsResponse questionsResponse = objectMapper.readValue(TestData.expectedResult, QuestionsResponse.class);
         when(mockExperianService.getQuestions("person-identity")).thenReturn(questionsResponse);
-        when(mockQuestionState.setQuestionsResponse(questionsResponse)).thenReturn(true);
 
-        when(mockapiGatewayProxyResponseEvent.getBody()).thenReturn(expectedResult);
+//        String state = objectMapper.writeValueAsString(questionState);
+//        LOGGER.info("questionState:" + state);
+//        storageService.update(
+//                sessionId,
+//                state,
+//                questionState.getControl().getAuthRefNo(),
+//                questionState.getControl().getURN());
+//        nextQuestion = questionState.getNextQuestion();
+//        responseBody = objectMapper.writeValueAsString(nextQuestion.get());
+//
+//        String newState = mockObjectMapper.writeValueAsString(mockQuestionState);
+        mockStorageService.update(anyString(), anyString(), anyString(), anyString());
+//        when(mockQuestionState.getNextQuestion()).thenReturn(Optional.ofNullable(new Question()));
+
+//        when(mockQuestionState.setQuestionsResponse(questionsResponse)).thenReturn(true);
+
+//        when(mockapiGatewayProxyResponseEvent.getBody()).thenReturn(expectedResult);
 
          mockapiGatewayProxyResponseEvent = questionHandler.handleRequest(input, mock(Context.class));
 
@@ -91,6 +104,50 @@ public class QuestionHandlerTest {
 //                "application/json"));
 //        verify(mockApiGatewayProxyResponseEvent).withStatusCode(HttpStatus.SC_OK);
 //        assertTrue(apiGatewayProxyResponseEvent.getBody().contains("Q007"));
+        System.out.println(mockapiGatewayProxyResponseEvent.getBody());
         assertEquals( HttpStatus.SC_OK, mockapiGatewayProxyResponseEvent.getStatusCode());
+    }
+
+    @Test
+    void ola_shouldReturn200OkWhen1stCalledAndReturn1stUnAnsweredQuestion() throws IOException, InterruptedException {
+        APIGatewayProxyRequestEvent input = mock(APIGatewayProxyRequestEvent.class);
+        Map<String, String> sessionHeader = Map.of(HEADER_SESSION_ID, "new-session-id");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Context contextMock = mock(Context.class);
+        KBVSessionItem kbvSessionItemMock = mock(KBVSessionItem.class);
+        PersonIdentity personIdentityMock = mock(PersonIdentity.class);
+//        QuestionState questionStateMock = mock(QuestionState.class);
+
+        QuestionState initialQuestionState = objectMapper.readValue(TestData.INITIAL_QUESTION_STATE, QuestionState.class);
+        when(input.getHeaders()).thenReturn(sessionHeader);
+        when(mockStorageService.getSessionId(sessionHeader.get(HEADER_SESSION_ID))).thenReturn(kbvSessionItemMock);
+        when(mockObjectMapper.readValue(kbvSessionItemMock.getUserAttributes(), PersonIdentity.class)).thenReturn(personIdentityMock);
+        when(mockObjectMapper.readValue(kbvSessionItemMock.getQuestionState(), QuestionState.class)).thenReturn(initialQuestionState);
+        when(mockObjectMapper.writeValueAsString(personIdentityMock)).thenReturn("person-identity");
+
+
+
+//        when(questionStateMock.getNextQuestion()).thenReturn(Optional.empty());
+
+        QuestionsResponse questionsResponse = objectMapper.readValue(TestData.expectedResult, QuestionsResponse.class);
+        when(mockExperianService.getQuestions("person-identity")).thenReturn(questionsResponse);
+//        when(questionStateMock.setQuestionsResponse(questionsResponse)).thenReturn(true);
+
+        QuestionState questionState = objectMapper.readValue(TestData.QUESTION_STATE, QuestionState.class);
+        when(mockObjectMapper.writeValueAsString(questionState)).thenReturn(TestData.QUESTION_STATE);
+//        Control controlMock = mock(Control.class);
+//        when(questionStateMock.getControl()).thenReturn(controlMock);
+//        when(controlMock.getAuthRefNo()).thenReturn(questionState.getControl().getAuthRefNo());
+//        when(controlMock.getURN()).thenReturn(questionState.getControl().getURN());
+        doNothing().when(mockStorageService).update(sessionHeader.get(HEADER_SESSION_ID), TestData.QUESTION_STATE, questionState.getControl().getAuthRefNo(), questionState.getControl().getURN());
+
+        Question nextQuestion = objectMapper.readValue(TestData.EXPECTED_QUESTION, Question.class);
+//        when(questionStateMock.getNextQuestion()).thenReturn(Optional.ofNullable(nextQuestion));
+
+//        when(mockObjectMapper.writeValueAsString(nextQuestion)).thenReturn(TestData.EXPECTED_QUESTION);
+
+        APIGatewayProxyResponseEvent response = questionHandler.handleRequest(input, contextMock);
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+        //assertEquals(null, response.getBody());
     }
 }
--- lambdas/question/src/test/java/uk.gov.di.ipv.cri.kbv.api.handler/TestData.java
+++ lambdas/question/src/test/java/uk.gov.di.ipv.cri.kbv.api.handler/TestData.java
@@ -17,6 +17,79 @@ public final class TestData {
                     + "      ]\n"
                     + "    }";
     ;
-    public static final String EXPECTED_QUESTION_STATE = "{\"control\":{\"authRefNo\":\"7CASD7WA7B\",\"urn\":\"50caf0fa-84f3-44af-98c8-5ca88460cb83\",\"dateTime\":null,\"testDatabase\":\"A\",\"clientAccountNo\":\"J8193\",\"clientBranchNo\":null,\"operatorID\":\"GDSCABINETUIIQ01U\",\"parameters\":{\"oneShotAuthentication\":\"N\",\"storeCaseData\":\"P\"}},\"qaPairs\":[{\"question\":{\"questionID\":\"Q00039\",\"text\":\"What is the balance, including interest, of your  loan?\",\"tooltip\":\"The approximate amount in £s on a current active personal loan. Does not include HP Loans, 2nd Mortgages or Home Credit.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £6,250\",\"OVER £6,250 UP TO £6,500\",\"OVER £6,500 UP TO £6,750\",\"OVER £6,750 UP TO £7,000\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}},\"answer\":null},{\"question\":{\"questionID\":\"Q00042\",\"text\":\"How much is your monthly loan payment?\",\"tooltip\":\"The approximate amount, in £s, that you should pay each month, not the amount that you actually pay. Personal Loans only, does not include HP Loans, 2nd Mortgages or Home Credit.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £500\",\"OVER £500 UP TO £550\",\"OVER £550 UP TO £600\",\"OVER £600 UP TO £650\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}},\"answer\":null}],\"nextQuestion\":{\"empty\":false,\"present\":true}}";
+    public static final String INITIAL_QUESTION_STATE = "{\"control\":null,\"qaPairs\":[],\"nextQuestion\":{\"empty\":true,\"present\":false}}";
 
+    public static  final String expectedResult = "{\n" +
+            "    \"control\": {\n" +
+            "        \"urn\": \"77597552-19e4-4a49-a350-787506f14964\",\n" +
+            "        \"authRefNo\": \"7CBSZJC44B\",\n" +
+            "        \"dateTime\": null,\n" +
+            "        \"testDatabase\": \"A\",\n" +
+            "        \"clientAccountNo\": \"J8193\",\n" +
+            "        \"clientBranchNo\": null,\n" +
+            "        \"operatorID\": \"GDSCABINETUIIQ01U\",\n" +
+            "        \"parameters\": {\n" +
+            "            \"oneShotAuthentication\": \"N\",\n" +
+            "            \"storeCaseData\": \"P\"\n" +
+            "        }\n" +
+            "    },\n" +
+            "    \"questions\": {\n" +
+            "        \"question\": [\n" +
+            "            {\n" +
+            "                \"questionID\": \"Q00036\",\n" +
+            "                \"text\": \"In which year did you open your loan?\",\n" +
+            "                \"tooltip\": \"Your current, active, personal loan. Does not include HP Loans, 2nd Mortgages or Home Credit.\",\n" +
+            "                \"answerFormat\": {\n" +
+            "                    \"identifier\": \"A00004\",\n" +
+            "                    \"fieldType\": \"G \",\n" +
+            "                    \"answerList\": [\n" +
+            "                        \"2022\",\n" +
+            "                        \"2016\",\n" +
+            "                        \"2017\",\n" +
+            "                        \"2015\",\n" +
+            "                        \"NONE OF THE ABOVE / DOES NOT APPLY\"\n" +
+            "                    ]\n" +
+            "                },\n" +
+            "                \"answerHeldFlag\": null\n" +
+            "            },\n" +
+            "            {\n" +
+            "                \"questionID\": \"Q00042\",\n" +
+            "                \"text\": \"How much is your monthly loan payment?\",\n" +
+            "                \"tooltip\": \"The approximate amount, in £s, that you should pay each month, not the amount that you actually pay. Personal Loans only, does not include HP Loans, 2nd Mortgages or Home Credit.\",\n" +
+            "                \"answerFormat\": {\n" +
+            "                    \"identifier\": \"A00004\",\n" +
+            "                    \"fieldType\": \"G \",\n" +
+            "                    \"answerList\": [\n" +
+            "                        \"UP TO £400\",\n" +
+            "                        \"OVER £400 UP TO £450\",\n" +
+            "                        \"OVER £450 UP TO £500\",\n" +
+            "                        \"OVER £500 UP TO £550\",\n" +
+            "                        \"NONE OF THE ABOVE / DOES NOT APPLY\"\n" +
+            "                    ]\n" +
+            "                },\n" +
+            "                \"answerHeldFlag\": null\n" +
+            "            }\n" +
+            "        ],\n" +
+            "        \"skipsRemaining\": null,\n" +
+            "        \"skipWarning\": null\n" +
+            "    },\n" +
+            "    \"results\": {\n" +
+            "        \"outcome\": \"Authentication Questions returned\",\n" +
+            "        \"authenticationResult\": null,\n" +
+            "        \"questions\": null,\n" +
+            "        \"alerts\": null,\n" +
+            "        \"nextTransId\": {\n" +
+            "            \"string\": [\n" +
+            "                \"RTQ\"\n" +
+            "            ]\n" +
+            "        },\n" +
+            "        \"caseFoundFlag\": null,\n" +
+            "        \"confirmationCode\": null\n" +
+            "    },\n" +
+            "    \"error\": null\n" +
+            "}";
+
+    public static final String QUESTION_STATE = "{\"control\":{\"authRefNo\":\"7CASCCWAN9\",\"urn\":\"56f62b11-8bcc-4836-a705-aed23d2ad97a\",\"dateTime\":null,\"testDatabase\":\"A\",\"clientAccountNo\":\"J8193\",\"clientBranchNo\":null,\"operatorID\":\"GDSCABINETUIIQ01U\",\"parameters\":{\"oneShotAuthentication\":\"N\",\"storeCaseData\":\"P\"}},\"qaPairs\":[{\"question\":{\"questionID\":\"Q00015\",\"text\":\"What is the outstanding balance of your current mortgage?\",\"tooltip\":\"The approximate amount in £s, including interest. A loan to buy property (or land) where the loan is secured by a charge on that property.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £60,000\",\"OVER £60,000 UP TO £85,000\",\"OVER £85,000 UP TO £110,000\",\"OVER £110,000 UP TO £135,000\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}},\"answer\":null},{\"question\":{\"questionID\":\"Q00018\",\"text\":\"How much is your  contracted monthly mortgage payment?\",\"tooltip\":\"The approximate amount, in £s, that you should pay each month, not the amount that you actually pay. This amount could have changed since the start of the mortgage. A loan to buy property (or land) where the loan is secured by a charge on that property.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £600\",\"OVER £600 UP TO £700\",\"OVER £700 UP TO £800\",\"OVER £800 UP TO £900\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}},\"answer\":null}],\"nextQuestion\":{\"empty\":false,\"present\":true}}";
+
+    public static final String EXPECTED_QUESTION = "{\"questionID\":\"Q00015\",\"text\":\"What is the outstanding balance of your current mortgage?\",\"tooltip\":\"The approximate amount in £s, including interest. A loan to buy property (or land) where the loan is secured by a charge on that property.\",\"answerHeldFlag\":null,\"answerFormat\":{\"identifier\":\"A00004\",\"fieldType\":\"G \",\"answerList\":[\"UP TO £60,000\",\"OVER £60,000 UP TO £85,000\",\"OVER £85,000 UP TO £110,000\",\"OVER £110,000 UP TO £135,000\",\"NONE OF THE ABOVE / DOES NOT APPLY\"]}}";
 }
