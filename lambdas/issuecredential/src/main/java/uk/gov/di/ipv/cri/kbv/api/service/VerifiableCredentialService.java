d66d0003737f4f42503cf265c4c6e3c6e029feaa
Stuart Foulds-Duncan <stuart.foulds-duncan@hippodigital.co.uk>
2022-06-08 01:13:54 +0100

    Fix error for mapping of List<Address> to Object[] to make compatible with Nimbus claimset

--- lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/VerifiableCredentialService.java
+++ lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/VerifiableCredentialService.java
@@ -1,11 +1,14 @@
 package uk.gov.di.ipv.cri.kbv.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import org.apache.logging.log4j.LogManager;
+import uk.gov.di.ipv.cri.common.library.domain.personidentity.Address;
 import uk.gov.di.ipv.cri.common.library.domain.personidentity.BirthDate;
 import uk.gov.di.ipv.cri.common.library.domain.personidentity.PersonIdentityDetailed;
 import uk.gov.di.ipv.cri.common.library.service.ConfigurationService;
@@ -19,7 +22,6 @@ import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static com.nimbusds.jwt.JWTClaimNames.EXPIRATION_TIME;
 import static com.nimbusds.jwt.JWTClaimNames.ISSUER;
@@ -80,7 +82,9 @@ public class VerifiableCredentialService {
                                         new String[] {W3_BASE_CONTEXT, DI_CONTEXT},
                                         VC_CREDENTIAL_SUBJECT,
                                         Map.of(
-                                                VC_ADDRESS_KEY, personIdentity.getAddresses(),
+                                                VC_ADDRESS_KEY,
+                                                        convertAddresses(
+                                                                personIdentity.getAddresses()),
                                                 VC_NAME_KEY, personIdentity.getNames(),
                                                 VC_BIRTHDATE_KEY,
                                                         convertBirthDates(
@@ -89,10 +93,24 @@ public class VerifiableCredentialService {
                                         calculateEvidence(kbvItem)))
                         .build();
 
+        var logger = LogManager.getLogger();
+
+        try {
+            logger.error(objectMapper.writeValueAsString(calculateEvidence(kbvItem)));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
         return signedJwtFactory.createSignedJwt(claimsSet);
     }
 
-    private List<Map<String, String>> convertBirthDates(List<BirthDate> birthDates) {
+    private Object[] convertAddresses(List<Address> addresses) {
+        return addresses.stream()
+                .map(address -> objectMapper.convertValue(address, Map.class))
+                .toArray();
+    }
+
+    private Object[] convertBirthDates(List<BirthDate> birthDates) {
         return birthDates.stream()
                 .map(
                         birthDate ->
@@ -101,7 +119,7 @@ public class VerifiableCredentialService {
                                         birthDate
                                                 .getValue()
                                                 .format(DateTimeFormatter.ISO_LOCAL_DATE)))
-                .collect(Collectors.toList());
+                .toArray();
     }
 
     private Map[] calculateEvidence(KBVItem kbvItem) {
