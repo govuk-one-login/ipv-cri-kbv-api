22f8a3a3586aeccf8ab306f0ce7b5451fcaadac0
Praveen Reddy <praveen.reddy@digital.cabinet-office.gov.uk>
2022-08-26 14:24:20 +0100

    OJ-862: Refactor to use DataStore class
    Use CreateItems Method in DataStore
    which now has a createItems for working
    with creating a batch of items.
    see: https://github.com/alphagov/di-ipv-cri-lib/tree/OJ-682-add-method-to-persist-multiple-items
    
    Co-authored-by: Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
    Co-authored-by: Tal Nagra <tal.Nagra@digital.cabinet-office.gov.uk>

--- lib/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/KBVAnswerStorageService.java
+++ lib/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/KBVAnswerStorageService.java
@@ -1,57 +1,34 @@
 package uk.gov.di.ipv.cri.kbv.api.service;
 
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteItemEnhancedRequest;
-import software.amazon.awssdk.enhanced.dynamodb.model.WriteBatch;
+import uk.gov.di.ipv.cri.common.library.annotations.ExcludeFromGeneratedCoverageReport;
+import uk.gov.di.ipv.cri.common.library.persistence.DataStore;
 import uk.gov.di.ipv.cri.common.library.persistence.DynamoDbEnhancedClientFactory;
 import uk.gov.di.ipv.cri.common.library.service.ConfigurationService;
 import uk.gov.di.ipv.cri.kbv.api.domain.KBVAnswerItem;
 import uk.gov.di.ipv.cri.kbv.api.domain.QuestionsResponse;
 import uk.gov.di.ipv.cri.kbv.api.util.KBVAnswerStorageMapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class KBVAnswerStorageService {
-    private final DynamoDbEnhancedClient enhancedClient;
+    private final DataStore<KBVAnswerItem> dataStore;
     private final KBVAnswerStorageMapper kbvAnswerStorageMapper;
-    private final String tableName;
 
+    @ExcludeFromGeneratedCoverageReport
     public KBVAnswerStorageService(
-            DynamoDbEnhancedClient enhancedClient,
-            KBVAnswerStorageMapper kbvAnswerStorageMapper,
-            String tableName) {
-        this.enhancedClient = enhancedClient;
+            DataStore<KBVAnswerItem> dataStore, KBVAnswerStorageMapper kbvAnswerStorageMapper) {
+        this.dataStore = dataStore;
         this.kbvAnswerStorageMapper = kbvAnswerStorageMapper;
-        this.tableName = tableName;
     }
 
     public KBVAnswerStorageService(ConfigurationService configurationService) {
         this(
-                new DynamoDbEnhancedClientFactory().getClient(),
-                new KBVAnswerStorageMapper(),
-                configurationService.getParameterValue("KBVAnswerTableName"));
+                new DataStore<>(
+                        configurationService.getParameterValue("KBVAnswerTableName"),
+                        KBVAnswerItem.class,
+                        new DynamoDbEnhancedClientFactory().getClient()),
+                new KBVAnswerStorageMapper());
     }
 
     public void save(QuestionsResponse questionsResponse) {
-        DynamoDbTable<KBVAnswerItem> kbvAnswerTable =
-                enhancedClient.table(tableName, TableSchema.fromBean(KBVAnswerItem.class));
-
-        List<WriteBatch> writeBatchList =
-                kbvAnswerStorageMapper.mapToKBVAnswerItems(questionsResponse).stream()
-                        .map(
-                                kbvAnswerItem ->
-                                        WriteBatch.builder(KBVAnswerItem.class)
-                                                .mappedTableResource(kbvAnswerTable)
-                                                .addPutItem(r -> r.item(kbvAnswerItem))
-                                                .build())
-                        .collect(Collectors.toList());
-
-        BatchWriteItemEnhancedRequest batchWriteItemEnhancedRequest =
-                BatchWriteItemEnhancedRequest.builder().writeBatches(writeBatchList).build();
-
-        enhancedClient.batchWriteItem(batchWriteItemEnhancedRequest);
+        dataStore.createItems(kbvAnswerStorageMapper.mapToKBVAnswerItems(questionsResponse));
     }
 }
--- lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/service/KBVAnswerStorageServiceTest.java
+++ lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/service/KBVAnswerStorageServiceTest.java
@@ -1,67 +1,42 @@
 package uk.gov.di.ipv.cri.kbv.api.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.enhanced.dynamodb.mapper.BeanTableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteItemEnhancedRequest;
+import uk.gov.di.ipv.cri.common.library.persistence.DataStore;
+import uk.gov.di.ipv.cri.kbv.api.domain.KBVAnswerItem;
 import uk.gov.di.ipv.cri.kbv.api.domain.KbvQuestion;
 import uk.gov.di.ipv.cri.kbv.api.domain.QuestionsResponse;
 import uk.gov.di.ipv.cri.kbv.api.util.KBVAnswerStorageMapper;
 
-import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.di.ipv.cri.kbv.api.util.TestDataCreator.getExperianQuestionResponse;
 import static uk.gov.di.ipv.cri.kbv.api.util.TestDataCreator.getQuestionOne;
 import static uk.gov.di.ipv.cri.kbv.api.util.TestDataCreator.getQuestionTwo;
 
 @ExtendWith(MockitoExtension.class)
 class KBVAnswerStorageServiceTest {
-    @Mock DynamoDbEnhancedClient mockEnhancedClient;
+    @Mock DataStore mockDataStore;
     @Mock KBVAnswerStorageMapper mockKBVAnswerMapper;
-
-    private KBVAnswerStorageService kbvAnswerStorageService;
-
-    @BeforeEach
-    void setUp() {
-        kbvAnswerStorageService =
-                new KBVAnswerStorageService(
-                        mockEnhancedClient, mockKBVAnswerMapper, "KBVAnswerTable");
-    }
+    @InjectMocks private KBVAnswerStorageService kbvAnswerStorageService;
 
     @Test
     void shouldSaveQuestionAnswerBatches() {
         QuestionsResponse questionsResponse =
                 getExperianQuestionResponse(new KbvQuestion[] {getQuestionOne(), getQuestionTwo()});
 
+        List<KBVAnswerItem> mockKVBAnswerItems = mock(List.class);
+        when(mockKBVAnswerMapper.mapToKBVAnswerItems(questionsResponse))
+                .thenReturn(mockKVBAnswerItems);
         kbvAnswerStorageService.save(questionsResponse);
 
-        verify(mockEnhancedClient).table(eq("KBVAnswerTable"), any(BeanTableSchema.class));
         verify(mockKBVAnswerMapper).mapToKBVAnswerItems(questionsResponse);
-        verify(mockEnhancedClient).batchWriteItem(any(BatchWriteItemEnhancedRequest.class));
-    }
-
-    @Test
-    void shouldDoNothingWhenQuestionAnswerBatchesReceivesAnEmptyList() {
-        QuestionsResponse questionsResponse =
-                getExperianQuestionResponse(
-                        new KbvQuestion[] {new KbvQuestion(), new KbvQuestion()});
-
-        kbvAnswerStorageService.save(questionsResponse);
-
-        assertEquals(
-                Collections.emptyList(),
-                mockKBVAnswerMapper.mapToKBVAnswerItems(questionsResponse));
-        verify(mockEnhancedClient).table(eq("KBVAnswerTable"), any(BeanTableSchema.class));
-        verify(mockKBVAnswerMapper, times(2)).mapToKBVAnswerItems(questionsResponse);
-        verify(mockEnhancedClient).batchWriteItem(any(BatchWriteItemEnhancedRequest.class));
+        verify(mockDataStore).createItems(mockKVBAnswerItems);
     }
 }
