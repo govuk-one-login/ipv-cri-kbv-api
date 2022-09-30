6b864bf4223e6e7aa177766b0c6264d973154186
praveen-gds <109537434+praveen-gds@users.noreply.github.com>
2022-09-30 14:49:34 +0100

    BAU: Updated deploy.sh (#177)

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,13 @@
 set -eu
 ./gradlew
 sam validate -t infrastructure/lambda/template.yaml
-sam build -t infrastructure/lambda/template.yaml --config-env dev
-sam deploy -t infrastructure/lambda/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset
+sam build -t infrastructure/lambda/template.yaml --config-env dev         
+sam deploy -t infrastructure/lambda/template.yaml \
+   --config-env dev \
+   --config-file samconfig.toml \
+   --no-fail-on-empty-changeset \
+   --parameter-overrides   CodeSigningEnabled=false \
+   AuditEventNamePrefix=/common-cri-parameters/KbvAuditEventNamePrefix \
+   CriIdentifier=/common-cri-parameters/KbvCriIdentifier \
+   CommonStackName=kbv-common-cri-api 
+  
