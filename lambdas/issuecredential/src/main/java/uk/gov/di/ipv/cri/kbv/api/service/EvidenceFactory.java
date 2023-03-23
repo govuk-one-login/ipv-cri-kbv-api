93fa3e89a2a70ef12b1811e85056ac0d3e5cf5c7
Sarah Cronk <sarah.cronk@digital.cabinet-office.gov.uk>
2023-03-23 13:29:44 +0000

    OJ-1326 linting

--- lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/EvidenceFactory.java
+++ lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/kbv/api/service/EvidenceFactory.java
@@ -158,10 +158,10 @@ public class EvidenceFactory {
         var summary = kbvItem.getQuestionAnswerResultSummary();
         return Objects.nonNull(summary)
                 && ((VC_THIRD_PARTY_KBV_CHECK_NOT_AUTHENTICATED.equalsIgnoreCase(status)
-                        && summary.getAnsweredIncorrect() > 1)
+                            && summary.getAnsweredIncorrect() > 1)
                         || (VC_THIRD_PARTY_KBV_CHECK_UNABLE_TO_AUTHENTICATE.equalsIgnoreCase(status)
-                                && summary.getAnsweredIncorrect() > 0 && summary.getQuestionsAsked()
-                                        == 3));
+                                && summary.getAnsweredIncorrect() > 0
+                                && summary.getQuestionsAsked() == 3));
     }
 
     private void logVcScore(String result) {
