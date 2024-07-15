9180a28d1c310a6e16f20274959c5bcbeb1451fe
Suraj Kumar <suraj.kumar@digital.cabinet-office.gov.uk>
2024-07-15 11:58:25 +0100

    Revert "OJ-2638: Fix `expiryDate` TTL in `person-identity` table"

--- ts-lambdas/sleep-function/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "jest";
-import baseConfig from "../../jest.config.base";
-
-export default {
-  ...baseConfig,
-  displayName: "ts-lambdas/sleep-function",
-} satisfies Config;
--- ts-lambdas/time-function/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "jest";
-import baseConfig from "../../jest.config.base";
-
-export default {
-  ...baseConfig,
-  displayName: "ts-lambdas/time-function",
-} satisfies Config;
