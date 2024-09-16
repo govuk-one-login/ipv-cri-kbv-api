82388e5e88be1355c9bd0b60db6c52333eaa5e98
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-09-16 17:39:31 +0100

    OJ-2689: Modify AnswerResource
    Modify AnswerResource so it can handle multiple answer resources
    using the row number of the dataset as an identifier to select
    which AnswerResource to use.

--- integration-tests/src/test/java/gov/uk/kbv/api/util/AnswerResource.java
+++ integration-tests/src/test/java/gov/uk/kbv/api/util/AnswerResource.java
@@ -13,18 +13,23 @@ import java.util.stream.Collectors;
 public class AnswerResource {
     private static final String CORRECT = "CORRECT";
     private static final String INCORRECT = "INCORRECT";
-    private static final String KENNETH_DECERQUEIRA = "KennethDecerqueira.json";
+    private static final Map<Integer, String> resourceMap =
+            Map.of(
+                    197, "KennethDecerqueira.json",
+                    256, "HildaHead.json");
 
-    public static String getCorrect(String question) throws IOException {
-        return AnswerResource.retrieve(question, CORRECT);
+    public static String getCorrect(String question, int user) throws IOException {
+        return AnswerResource.retrieve(question, CORRECT, user);
     }
 
-    public static String getInCorrect(String question) throws IOException {
-        return AnswerResource.retrieve(question, INCORRECT);
+    public static String getInCorrect(String question, int user) throws IOException {
+        return AnswerResource.retrieve(question, INCORRECT, user);
     }
 
-    private static String retrieve(String question, String answerState) throws IOException {
-        return Arrays.stream(convert(KENNETH_DECERQUEIRA).get(question).toString().split(";"))
+    private static String retrieve(String question, String answerState, int user)
+            throws IOException {
+        String jsonFileName = getResourceFileName(user);
+        return Arrays.stream(convert(jsonFileName).get(question).toString().split(";"))
                 .filter(item -> item.startsWith(answerState))
                 .collect(Collectors.joining())
                 .split(":")[1];
@@ -38,5 +43,13 @@ public class AnswerResource {
         Map<String, Object> map = mapper.readValue(reader, Map.class);
 
         return map;
+    }
+
+    private static String getResourceFileName(int user) throws IOException {
+        String jsonFileName = resourceMap.get(user);
+        if (jsonFileName == null) {
+            throw new IOException("Resource ID not found: " + user);
+        }
+        return jsonFileName;
     }
 }
