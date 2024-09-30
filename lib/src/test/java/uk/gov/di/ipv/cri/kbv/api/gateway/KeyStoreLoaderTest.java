55e79c09cacf2e2f3a2b4ab851fcf2820ad72bf8
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-09-30 09:28:57 +0100

    INCIDEN-937: Add more test coverage for KeyStoreLoader
    Stop returning null when getKeyStorePath fails, let the error
    automatically bubble up this will be handled upstream components
    like the lambda(s) handlers

--- lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/gateway/KeyStoreLoaderTest.java
+++ lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/gateway/KeyStoreLoaderTest.java
@@ -7,30 +7,35 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.di.ipv.cri.common.library.service.ConfigurationService;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class KeyStoreLoaderTest {
+    final String base64KeyStorePassword = "keystore-password";
+    final String keystoreValue = "a2V5c3RvcmUtdmFsdWU=";
     @Mock private ConfigurationService mockConfigurationService;
     private KeyStoreLoader keyStoreLoader;
 
     @BeforeEach
     void setUp() {
         this.keyStoreLoader = new KeyStoreLoader(mockConfigurationService);
+        when(mockConfigurationService.getSecretValue("experian/keystore"))
+                .thenReturn(keystoreValue);
     }
 
     @Test
-    void loadShouldSetupSystemProperties() {
-        final String base64KeyStorePassword = "keystore-password";
-        final String keystoreValue = "a2V5c3RvcmUtdmFsdWU=";
-        when(mockConfigurationService.getSecretValue("experian/keystore"))
-                .thenReturn(keystoreValue);
+    void loadShouldSetupSystemProperties() throws IOException {
         when(mockConfigurationService.getSecretValue("experian/keystore-password"))
                 .thenReturn(base64KeyStorePassword);
+
         keyStoreLoader.load();
+
         verify(mockConfigurationService).getSecretValue("experian/keystore");
         verify(mockConfigurationService).getSecretValue("experian/keystore-password");
 
@@ -39,5 +44,36 @@ class KeyStoreLoaderTest {
         assertTrue(keyStoreSysProperty.startsWith(System.getProperty("java.io.tmpdir")));
         assertTrue(keyStoreSysProperty.endsWith(".tmp"));
         assertEquals(base64KeyStorePassword, System.getProperty("javax.net.ssl.keyStorePassword"));
+    }
+
+    @Test
+    void shouldHandleNullKeystoreSecretValueGracefully() {
+        when(mockConfigurationService.getSecretValue("experian/keystore")).thenReturn(null);
+
+        IllegalStateException exception =
+                assertThrows(
+                        IllegalStateException.class,
+                        () -> {
+                            keyStoreLoader.load();
+                        });
+
+        assertEquals("Persist keystore to file failed: null", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionWhenKeystorePathFails() {
+        when(mockConfigurationService.getSecretValue("experian/keystore"))
+                .thenReturn("wrong keystore");
+
+        IllegalStateException exception =
+                assertThrows(
+                        IllegalStateException.class,
+                        () -> {
+                            keyStoreLoader.load();
+                        });
+
+        assertEquals(
+                "Persist keystore to file failed: Illegal base64 character 20",
+                exception.getMessage());
     }
 }
