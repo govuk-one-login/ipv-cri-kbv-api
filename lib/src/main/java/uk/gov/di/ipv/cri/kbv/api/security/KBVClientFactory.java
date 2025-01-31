47c20c832be8fae68692d3930d7dcf681bed05f5
Suraj Kumar <sk96.uk@gmail.com>
2025-01-31 10:37:23 +0000

    feat: add span for createClient
    refs: OJ-3015

--- lib/src/main/java/uk/gov/di/ipv/cri/kbv/api/security/KBVClientFactory.java
+++ lib/src/main/java/uk/gov/di/ipv/cri/kbv/api/security/KBVClientFactory.java
@@ -2,8 +2,10 @@ package uk.gov.di.ipv.cri.kbv.api.security;
 
 import com.experian.uk.schema.experian.identityiq.services.webservice.IdentityIQWebService;
 import com.experian.uk.schema.experian.identityiq.services.webservice.IdentityIQWebServiceSoap;
+import io.opentelemetry.api.trace.Span;
 import uk.gov.di.ipv.cri.common.library.service.ConfigurationService;
 import uk.gov.di.ipv.cri.kbv.api.exception.InvalidSoapTokenException;
+import uk.gov.di.ipv.cri.kbv.api.util.OpenTelemetryUtil;
 
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.WebServiceException;
@@ -24,6 +26,14 @@ public class KBVClientFactory {
     }
 
     public IdentityIQWebServiceSoap createClient() {
+        Span span =
+                OpenTelemetryUtil.createSpan(
+                        this.getClass(),
+                        "createClient",
+                        "IdentityIQWebServiceSoap createClient",
+                        "_UNKNOWN",
+                        "http://schema.uk.experian.com/Experian/IdentityIQ/Services/WebService");
+
         try {
             identityIQWebService.setHandlerResolver(headerHandlerResolver);
 
@@ -36,13 +46,18 @@ public class KBVClientFactory {
                             BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
                             configurationService.getSecretValue("experian/iiq-webservice"));
 
+            OpenTelemetryUtil.endSpan(span);
+
             return identityIQWebServiceSoap;
 
         } catch (SOAPFaultException e) {
+            OpenTelemetryUtil.endSpanWithError(span);
             throw new InvalidSoapTokenException("SOAP Fault occurred: " + e.getMessage());
         } catch (WebServiceException e) {
+            OpenTelemetryUtil.endSpanWithError(span);
             throw new InvalidSoapTokenException("Web Service error occurred: " + e.getMessage());
         } catch (Exception e) {
+            OpenTelemetryUtil.endSpanWithError(span);
             throw new InvalidSoapTokenException("Unexpected error occurred: " + e.getMessage());
         }
     }
