46cbf355ae57f42647dda1474ecf6b3ddfbc409b
Suraj Kumar <sk96.uk@gmail.com>
2025-06-20 16:35:42 +0100

    wip

--- lambdas/healthcheck/src/main/java/uk/gov/di/ipv/cri/kbv/healthcheck/handler/config/ExperianSecrets.java
+++ lambdas/healthcheck/src/main/java/uk/gov/di/ipv/cri/kbv/healthcheck/handler/config/ExperianSecrets.java
@@ -4,28 +4,25 @@ import uk.gov.di.ipv.cri.common.library.service.ConfigurationService;
 import uk.gov.di.ipv.cri.common.library.util.ClientProviderFactory;
 
 public class ExperianSecrets {
-    private final ConfigurationService configurationService;
+   /// private final ConfigurationService configurationService;
 
     public ExperianSecrets() {
-        this(new ClientProviderFactory());
+        //this(new ClientProviderFactory());
     }
 
     public ExperianSecrets(ClientProviderFactory clientProviderFactory) {
-        this.configurationService =
-                new ConfigurationService(
-                        clientProviderFactory.getSSMProvider(),
-                        clientProviderFactory.getSecretsProvider());
+//        this.configurationService =
+//                new ConfigurationService(
+//                        clientProviderFactory.getSSMProvider(),
+//                        clientProviderFactory.getSecretsProvider());
     }
 
     public String getWaspUrl() {
-        return configurationService.getSecretValue(Configuration.WASP_URL_SECRET);
     }
 
     public String getKeystorePassword() {
-        return configurationService.getSecretValue(Configuration.KEYSTORE_PASSWORD);
     }
 
     public String getKeystoreSecret() {
-        return configurationService.getSecretValue(Configuration.KEYSTORE_SECRET);
     }
 }
