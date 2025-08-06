fbc1c6c61533efe1f915b61deaf7d439d812226c
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2025-08-06 16:28:42 +0100

    OJ-3092: Params are being fetched in lambda

--- lambdas/abandon/src/main/java/uk/gov/di/ipv/cri/kbv/api/handler/AbandonKbvHandler.java
+++ lambdas/abandon/src/main/java/uk/gov/di/ipv/cri/kbv/api/handler/AbandonKbvHandler.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.lambda.powertools.logging.CorrelationIdPathConstants;
 import software.amazon.lambda.powertools.logging.Logging;
@@ -13,8 +12,6 @@ import software.amazon.lambda.powertools.parameters.AppConfigProvider;
 import software.amazon.lambda.powertools.parameters.ParamManager;
 import software.amazon.lambda.powertools.tracing.Tracing;
 import uk.gov.di.ipv.cri.common.library.annotations.ExcludeFromGeneratedCoverageReport;
-import uk.gov.di.ipv.cri.common.library.domain.AuditEventContext;
-import uk.gov.di.ipv.cri.common.library.exception.SqsException;
 import uk.gov.di.ipv.cri.common.library.service.AuditService;
 import uk.gov.di.ipv.cri.common.library.service.SessionService;
 import uk.gov.di.ipv.cri.common.library.util.EventProbe;
@@ -22,10 +19,6 @@ import uk.gov.di.ipv.cri.kbv.api.service.KBVStorageService;
 import uk.gov.di.ipv.cri.kbv.api.service.ServiceFactory;
 
 import java.util.Map;
-import java.util.UUID;
-
-import static org.apache.logging.log4j.Level.ERROR;
-import static uk.gov.di.ipv.cri.kbv.api.domain.IIQAuditEventType.ABANDONED;
 
 public class AbandonKbvHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
@@ -59,20 +52,6 @@ public class AbandonKbvHandler
                         serviceFactory.getDynamoDbEnhancedClient());
         this.sessionService = serviceFactory.getSessionService();
         this.auditService = serviceFactory.getAuditService();
-
-        var applicationId = System.getenv("APP_CONFIG_ID");
-        var environmentId = System.getenv("APP_CONFIG_ENVIRONMENT_ID");
-
-        System.out.println(applicationId);
-        System.out.println(environmentId);
-
-        AppConfigProvider appConfigProvider =
-                ParamManager.getAppConfigProvider(environmentId, applicationId);
-
-        String value = appConfigProvider.get("foo");
-
-        System.out.println();
-        System.out.println("foo value: " + value);
     }
 
     @Override
@@ -82,32 +61,53 @@ public class AbandonKbvHandler
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+
+        var applicationId = System.getenv("APP_CONFIG_ID");
+        var environmentId = System.getenv("APP_CONFIG_ENVIRONMENT_ID");
+        var profileId = System.getenv("APP_CONFIG_PROFILE_ID");
+
+        System.out.println("hi");
+        System.out.println(applicationId);
+        System.out.println(environmentId);
+
+        AppConfigProvider appConfigProvider =
+                ParamManager.getAppConfigProvider(environmentId, applicationId);
+        // .withMaxAge(3, MINUTES);
+
+        String paramsRaw = appConfigProvider.get(profileId);
+
+        System.out.println();
+        System.out.println("paramsRaw: " + paramsRaw);
+
         response.withHeaders(Map.of("Content-Type", "application/json"));
-        try {
-            var sessionHeader = input.getHeaders().get(HEADER_SESSION_ID);
-            var sessionId = UUID.fromString(sessionHeader);
-            var kbvItem = kbvStorageService.getKBVItem(sessionId);
-            kbvItem.setStatus(ABANDON_STATUS);
-            kbvStorageService.update(kbvItem);
+        // try {
+        //     var sessionHeader = input.getHeaders().get(HEADER_SESSION_ID);
+        //     var sessionId = UUID.fromString(sessionHeader);
+        //     var kbvItem = kbvStorageService.getKBVItem(sessionId);
+        //     kbvItem.setStatus(ABANDON_STATUS);
+        //     kbvStorageService.update(kbvItem);
 
-            var sessionItem = sessionService.validateSessionId(sessionId.toString());
-            sessionService.createAuthorizationCode(sessionItem);
+        //     var sessionItem = sessionService.validateSessionId(sessionId.toString());
+        //     sessionService.createAuthorizationCode(sessionItem);
 
-            response.withStatusCode(HttpStatusCode.OK);
-            eventProbe.counterMetric(ABANDON_KBV);
+        //     response.withStatusCode(HttpStatusCode.OK);
+        //     eventProbe.counterMetric(ABANDON_KBV);
 
-            auditService.sendAuditEvent(
-                    ABANDONED.toString(), new AuditEventContext(input.getHeaders(), sessionItem));
+        //     auditService.sendAuditEvent(
+        //             ABANDONED.toString(), new AuditEventContext(input.getHeaders(),
+        // sessionItem));
 
-            return response;
+        //     return response;
 
-        } catch (NullPointerException | SqsException npe) {
-            response.withStatusCode(HttpStatusCode.BAD_REQUEST);
-            eventProbe.log(ERROR, npe).counterMetric(ABANDON_KBV, 0d);
-        } catch (AwsServiceException e) {
-            response.withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR);
-            eventProbe.log(ERROR, e).counterMetric(ABANDON_KBV, 0d);
-        }
-        return response;
+        // } catch (NullPointerException | SqsException npe) {
+        //     response.withStatusCode(HttpStatusCode.BAD_REQUEST);
+        //     eventProbe.log(ERROR, npe).counterMetric(ABANDON_KBV, 0d);
+        // } catch (AwsServiceException e) {
+        //     response.withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR);
+        //     eventProbe.log(ERROR, e).counterMetric(ABANDON_KBV, 0d);
+        // }
+        // return response;
+
+        return response.withStatusCode(HttpStatusCode.OK);
     }
 }
