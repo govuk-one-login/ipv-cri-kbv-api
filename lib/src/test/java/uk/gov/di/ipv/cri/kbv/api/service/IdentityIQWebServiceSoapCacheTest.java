a84c54ad82387bc55d1cc12f92a730ac66d87d7d
MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>
2026-01-16 16:29:25 +0000

    OJ-3546: fix - remove timeout implementation from gateway to soap service based on GH comments with associated tests

--- lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/service/IdentityIQWebServiceSoapCacheTest.java
+++ lib/src/test/java/uk/gov/di/ipv/cri/kbv/api/service/IdentityIQWebServiceSoapCacheTest.java
@@ -1,23 +1,31 @@
 package uk.gov.di.ipv.cri.kbv.api.service;
 
 import com.experian.uk.schema.experian.identityiq.services.webservice.IdentityIQWebServiceSoap;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.cri.kbv.api.exception.TimeoutException;
 import uk.gov.di.ipv.cri.kbv.api.security.KBVClientFactory;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class IdentityIQWebServiceSoapCacheTest {
     @Mock private ServiceFactory mockServiceFactory;
     @Mock private KBVClientFactory mockKbvClientFactory;
     @Mock private IdentityIQWebServiceSoap mockIdentityIQWebServiceSoap;
+    @Spy private IdentityIQWebServiceSoapCache identityIQWebServiceSoapCache;
 
     @BeforeEach
     void setUp() {
@@ -31,18 +39,59 @@ class IdentityIQWebServiceSoapCacheTest {
 
     @Test
     void shouldCache() {
-        IdentityIQWebServiceSoapCache cache = new IdentityIQWebServiceSoapCache();
-        IdentityIQWebServiceSoap retrieved = cache.get("dummy", mockServiceFactory);
+        doNothing()
+                .when(identityIQWebServiceSoapCache)
+                .implementTimeout(mockIdentityIQWebServiceSoap);
+        IdentityIQWebServiceSoap retrieved =
+                identityIQWebServiceSoapCache.get("dummy", mockServiceFactory);
 
+        assertNotNull(retrieved);
         assertEquals(mockIdentityIQWebServiceSoap, retrieved);
     }
 
     @Test
     void shouldUseExisting() {
-        IdentityIQWebServiceSoapCache cache = new IdentityIQWebServiceSoapCache();
-        IdentityIQWebServiceSoap first = cache.get("dummy", mockServiceFactory);
-        IdentityIQWebServiceSoap second = cache.get("dummy", mockServiceFactory);
+        doNothing()
+                .when(identityIQWebServiceSoapCache)
+                .implementTimeout(mockIdentityIQWebServiceSoap);
+        IdentityIQWebServiceSoap first =
+                identityIQWebServiceSoapCache.get("dummy", mockServiceFactory);
+        IdentityIQWebServiceSoap second =
+                identityIQWebServiceSoapCache.get("dummy", mockServiceFactory);
 
         assertEquals(first, second);
+    }
+
+    @Test
+    void shouldThrowTimeoutException() {
+        doThrow(new TimeoutException("timeout"))
+                .when(identityIQWebServiceSoapCache)
+                .implementTimeout(mockIdentityIQWebServiceSoap);
+
+        TimeoutException exception =
+                assertThrows(
+                        TimeoutException.class,
+                        () -> identityIQWebServiceSoapCache.get("dummy", mockServiceFactory));
+
+        assertEquals("timeout", exception.getMessage());
+    }
+
+    @Test
+    void shouldTestHttpConduitTimeoutMethod() throws TimeoutException {
+        Client mockClient = mock(Client.class);
+        HTTPConduit mockConduit = mock(HTTPConduit.class);
+
+        try (MockedStatic<ClientProxy> clientProxyMock = mockStatic(ClientProxy.class)) {
+
+            clientProxyMock
+                    .when(() -> ClientProxy.getClient(mockIdentityIQWebServiceSoap))
+                    .thenReturn(mockClient);
+
+            when(mockClient.getConduit()).thenReturn(mockConduit);
+
+            identityIQWebServiceSoapCache.get("dummy", mockServiceFactory);
+
+            verify(mockConduit).setClient(any(HTTPClientPolicy.class));
+        }
     }
 }
